7502837d09130f444017d072830c4cd89548e672
jmczerwinska <asia.frej@gmail.com>
2019-10-26 19:34:20 +0200

    add new method to Api

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ const Api = function () {
     this.url = 'https://api.github.com/repos/';
 }
 
-Api.prototype.getStars = function (user, repo) {
+Api.prototype.get = function (user, repo) {
     const url = this.url + user + '/' + repo;
-    return fetch(url, {method: 'GET'}).then(response => response.json()).then((response) => console.log(response['stargazers_count']));
+    return fetch(url, {
+        method: 'GET'
+    }).then(response => response.json());
 }
+
+Api.prototype.getStars = function (user, repo) {
+    try {
+        return this.get(user, repo).then((response) => console.log(response['stargazers_count']));
+    } catch (error) {
+        console.log(0)
+    }
 
 const api = new Api();
 
-const allegroApi = api.getStars('allegro', 'allegro-api').then((resp) => {console.log(resp)});
+const allegroApi = api.getStars('allegro', 'allegro-api').then((resp) => {
+    console.log(resp)
+});
 console.log(starCount);
 
-const sklep = api.getStars('jmczerwinska', 'sklep').then((resp) => {console.log(resp)});
+const sklep = api.getStars('jmczerwinska', 'sklep').then((resp) => {
+    console.log(resp)
+});
 console.log(sklep);
