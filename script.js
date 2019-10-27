aa5375b2bfa52eed8254d46a7b05cda3ded55180
jmczerwinska <asia.frej@gmail.com>
2019-10-27 15:07:42 +0100

    merge scripts in script.j

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-const Api = function () {
-    this.url = 'https://api.github.com/repos/';
+ const Api = function () {
+     this.url = 'https://api.github.com/repos/';
+ }
+
+ Api.prototype.handleResponse = function (response) {
+     if (response.status === 404) {
+         throw Error;
+     } else {
+         return response.json()
+     };
+ }
+
+ Api.prototype.get = function (user, repo) {
+     const url = this.url + user + '/' + repo;
+     return fetch(url).then(this.handleResponse);
+ }
+
+ Api.prototype.getStars = function (user, repo) {
+     return this.get(user, repo)
+         .then(response => {
+             count.innerHTML = response['stargazers_count'];
+             showAnswer();
+         }).catch(error => {
+             console.log(error);
+             alert('Nieprawidłowa nazwa użytkownika lub repozytorium');
+         })
+ }
+
+ const api = new Api();
+
+let user = document.querySelector('#user');
+let repo = document.querySelector('#repo');
+let answer = document.querySelector('#answer');
+let count = document.querySelector('#count');
+const button = document.querySelector('#get-stars');
+
+function checkInput(){
+    button.disabled = !(user.value !== '' && repo.value !== '');
+};
+
+const showAnswer = function(){
+    this.userName = document.querySelector('#user-name');
+    this.repoName = document.querySelector('#repo-name');
+    this.userName.innerHTML = user.value;
+    this.repoName.innerHTML = repo.value;
+    answer.style.display = 'block';
 }
 
-Api.prototype.handleResponse = function (response) {
-    if (response.status === 404) {
-       alert('Nieprawidłowa nazwa użytkownika lub repozytorium');
-    }
-    return response.json();
-}
-
-Api.prototype.getStars = function (user, repo) {
-    const url = this.url + user + '/' + repo;
-    return fetch(url)
-        .then(this.handleResponse)
-        .then(response => {
-                let stars = response['stargazers_count'];
-                count.innerHTML = stars;                
-            }
-        )
-}
+user.addEventListener('input', checkInput);
+repo.addEventListener('input', checkInput);
+button.addEventListener('click', function(){
+   api.getStars(user.value, repo.value);
+});
