c92e4567e155af42fc67d56224aa9e048c8f0119
jmczerwinska <asia.frej@gmail.com>
2019-11-01 19:42:13 +0100

    change handleResponse method

--- script.js
+++ script.js
@@ -5,9 +5,8 @@
  Api.prototype.handleResponse = function (response) {
      if (response.status === 404) {
          throw Error;
-     } else {
-         return response.json()
-     };
+     }
+     return response.json();
  }
 
  Api.prototype.get = function (user, repo) {
@@ -15,40 +14,40 @@
      return fetch(url).then(this.handleResponse);
  }
 
- Api.prototype.getStars = function (user, repo,callback) {
+ Api.prototype.getStars = function (user, repo, callback) {
      return this.get(user, repo)
-        .then(response => response['stargazers_count'])
-        .then(response => callback(response))
-        .catch(error => {
+         .then(response => response['stargazers_count'])
+         .then(response => callback(response))
+         .catch(error => {
              console.log(error);
              alert('Nieprawidłowa nazwa użytkownika lub repozytorium');
-        })
+         })
  }
 
  const api = new Api();
 
-const user = document.querySelector('#user');
-const repo = document.querySelector('#repo');
+ const user = document.querySelector('#user');
+ const repo = document.querySelector('#repo');
 
-const button = document.querySelector('#get-stars');
+ const button = document.querySelector('#get-stars');
 
-function checkInput(){
-    button.disabled = !(user.value !== '' && repo.value !== '');
-};
+ function checkInput() {
+     button.disabled = !(user.value !== '' && repo.value !== '');
+ };
 
-const showAnswer = function(str){
-    this.userName = document.querySelector('#user-name');
-    this.repoName = document.querySelector('#repo-name');
-    this.answer = document.querySelector('#answer');
-    this.count = document.querySelector('#count');
-    this.userName.innerHTML = user.value;
-    this.repoName.innerHTML = repo.value;
-    this.count.innerHTML = str;
-    this.answer.style.display = 'block';
-}
+ const showAnswer = function (str) {
+     this.userName = document.querySelector('#user-name');
+     this.repoName = document.querySelector('#repo-name');
+     this.answer = document.querySelector('#answer');
+     this.count = document.querySelector('#count');
+     this.userName.innerHTML = user.value;
+     this.repoName.innerHTML = repo.value;
+     this.count.innerHTML = str;
+     this.answer.style.display = 'block';
+ }
 
-user.addEventListener('input', checkInput);
-repo.addEventListener('input', checkInput);
-button.addEventListener('click', function(){
-   api.getStars(user.value, repo.value, showAnswer);
-});
+ user.addEventListener('input', checkInput);
+ repo.addEventListener('input', checkInput);
+ button.addEventListener('click', function () {
+     api.getStars(user.value, repo.value, showAnswer);
+ });
